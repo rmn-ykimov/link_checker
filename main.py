bf45c0d2d590d75901c58c875a23907f7c45fff6
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2023-01-23 15:37:08 +0400

    error handling improvement
    The `validate_link` function raises an exception if the link is not a valid link, and the `check_method_status` and `check_methods` functions raise exceptions if an error occurs while making the request or processing the response. Also, a global exception handler around the entire main function to catch any unhandled exceptions that may occur added.

--- main.py
+++ main.py
@@ -2,32 +2,45 @@ import json
 import re
 import requests
 
-
+# This function validates if the given link is valid, it is intended to use only for http and https protocols
 def validate_link(link):
+    # Regular expression pattern for http and https
     pattern = re.compile(r'^https?://')
-    return pattern.match(link) is not None
+    # check if the link is valid or not
+    if not pattern.match(link):
+        # If the link is not valid, raise an exception
+        raise Exception(f"The string {link} is not a valid link.")
+    return True
 
-
+# This function checks available methods on the given link
 def check_methods(link):
-    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS',
-               'TRACE', 'PATCH']
+    # List of all possible http methods
+    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+    # Initialize an empty list to store available methods
     available_methods = []
     for method in methods:
-        status = check_method_status(link, method)
-        if status != 405:
-            available_methods.append(method)
+        try:
+            status = check_method_status(link, method)
+            # if the status code is not 405, method is considered as available
+            if status != 405:
+                available_methods.append(method)
+        except Exception as e:
+            print(f"An error occurred: {e}")
     return available_methods
 
-
+# This function checks the status of the given method on the given link
 def check_method_status(link, method):
     try:
+        # send request to the link for the given method
         response = requests.request(method, link)
+        # check if the response status code is not 200
+        if response.status_code != 200:
+            # Raise an exception if the status code is not 200
+            raise Exception(f"Link returned status code {response.status_code}")
         return response.status_code
     except requests.exceptions.RequestException as e:
-        print(
-            f"An error occurred while checking {method} method for {link}. "
-            f"Error: {e}")
-
+        # Raise an exception if an error occurs while making the request
+        raise Exception(f"An error occurred while checking {method} method for {link}. Error: {e}")
 
 def main():
     links = input("Enter a list of links: ").split()
@@ -36,14 +49,14 @@ def main():
         return
     result = {}
     for link in links:
-        if not validate_link(link):
-            print(f"The string {link} is not a link.")
-            continue
-        result[link] = {}
-        for method in check_methods(link):
-            result[link][method] = check_method_status(link, method)
+        try:
+            validate_link(link)
+            result[link] = {}
+            for method in check_methods(link):
+                result[link][method] = check_method_status(link, method)
+        except Exception as e:
+            print(f"An error occurred: {e}")
     print(json.dumps(result, indent=4))
-
 
 if __name__ == "__main__":
     main()
