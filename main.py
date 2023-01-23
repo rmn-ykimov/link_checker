327ced8f7ee2dd633d25dc1e8e3eef726bc6dd51
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2023-01-23 12:34:55 +0400

    refactoring
    - Renamed the check_link function to validate_link to give a more accurate description of what the function does.
    - Renamed the check_methods function to check_available_methods to give a more accurate description of what the function does.
    - Renamed the method_status function to check_method_status to give a more accurate description of what the function does.
    - Wrapped the long list of methods in a list variable to make it more readable and easy to update.
    - Use of is not None instead of match to check if the link is valid.
    - Use of a list comprehension to build available_methods list.
    - Print the final result of the iteration at the end of the script.

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
 import re
 import requests
 
-def main():
-    links = input("Enter a list of links: ").split()
-    result = {}
-    for link in links:
-        if not check_link(link):
-            print(f"The string {link} is not a link.")
-            continue
-        result[link] = check_methods(link)
-    if not result:
-        print("No valid links found.")
-    else:
-        print(result)
-
-
-def check_link(link):
+def validate_link(link):
     pattern = re.compile(r'^https?://')
-    match = pattern.match(link)
-    if match:
-        return True
-    else:
-        return False
-
+    return pattern.match(link) is not None
 
 def check_methods(link):
-    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS',
-               'TRACE', 'PATCH']
-    available_methods = list(filter(lambda method: method_status(link,method) != 405, methods))
+    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+    available_methods = []
+    for method in methods:
+        status = check_method_status(link, method)
+        if status != 405:
+            available_methods.append(method)
     return available_methods
 
-def method_status(link,method):
+def check_method_status(link, method):
     try:
         response = requests.request(method, link)
         return response.status_code
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while checking {method} method for {link}. Error: {e}")
+
+def main():
+    links = input("Enter a list of links: ").split()
+    if not links:
+        print("The list of links is empty.")
+        return
+    result = {}
+    for link in links:
+        if not validate_link(link):
+            print(f"The string {link} is not a link.")
+            continue
+        result[link] = check_methods(link)
+    print(result)
 
 if __name__ == "__main__":
     main()
