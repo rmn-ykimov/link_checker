76f1f966cc3661fce6f486ff8a6be4ca5855bfb3
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2023-01-23 14:31:59 +0400

    json output addition
    Now each method have its own key-value pair, with the key being the method and the value being the status code.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import json
 import re
 import requests
+
 
 def validate_link(link):
     pattern = re.compile(r'^https?://')
     return pattern.match(link) is not None
 
+
 def check_methods(link):
-    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+    methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS',
+               'TRACE', 'PATCH']
     available_methods = []
     for method in methods:
         status = check_method_status(link, method)
@@ -14,12 +18,16 @@ def check_methods(link):
             available_methods.append(method)
     return available_methods
 
+
 def check_method_status(link, method):
     try:
         response = requests.request(method, link)
         return response.status_code
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred while checking {method} method for {link}. Error: {e}")
+        print(
+            f"An error occurred while checking {method} method for {link}. "
+            f"Error: {e}")
+
 
 def main():
     links = input("Enter a list of links: ").split()
@@ -31,8 +39,11 @@ def main():
         if not validate_link(link):
             print(f"The string {link} is not a link.")
             continue
-        result[link] = check_methods(link)
-    print(result)
+        result[link] = {}
+        for method in check_methods(link):
+            result[link][method] = check_method_status(link, method)
+    print(json.dumps(result, indent=4))
+
 
 if __name__ == "__main__":
     main()
